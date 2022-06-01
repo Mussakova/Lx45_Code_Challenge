a757d9894cbd6052aaaae50859764d439acd726b
Hirepan E <ginebrasyrel@gmail.com>
2022-06-01 13:04:55 -0500

    Lógica del requerimiento terminada y separada de las solicitudes del server

--- services/studentServices.js
+++ services/studentServices.js
@@ -1,3 +1,20 @@
-require("../db/visualpartners.json");
+const allStudents = require("../db/visualpartners.json");
 
+class studentService{
+    static allStudents(){
+        return allStudents;
+    }
+    static studentsEmailsWithCertification(){
+        const studentsWithCertification = allStudents.filter((i)=>{return i.haveCertification == true;});
+        const studentsEmailsWithCertification = studentsWithCertification.map((i)=> i.email);
+        return studentsEmailsWithCertification;
+    }
+    static studentsWithCreditsOver500(){
+        const studentsWithCreditsOver500 = allStudents.filter((i)=>{ return i.credits >= 500;});
+        return studentsWithCreditsOver500;
+    }
+
+}
+
+module.exports = studentService;
 
